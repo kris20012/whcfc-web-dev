00f172fbc9db99f120835dc66ea59194be661f01
Legus-Yeung <legus.yeung@gmail.com>
2024-09-22 11:20:42 -0400

    refined header link with proper a tag with href, routes.ts needs to be updated when there are components for same page navigation, for example about-us/who-we-are or league/scoreboard

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -1,6 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+// import { AboutUsComponent } from './components/about-us/about-us.component';
+// import { WhoWeAreComponent } from './components/about-us/who-we-are/who-we-are.component';
+// import { FaqsComponent } from './components/about-us/faqs/faqs.component';
+// import { LeagueComponent } from './components/league/league.component';
+// import { ScoreboardComponent } from './components/league/scoreboard/scoreboard.component';
+// import { RecentMatchComponent } from './components/league/recent-match/recent-match.component';
+// import { ScheduleComponent } from './components/league/schedule/schedule.component';
+// import { NewsComponent } from './components/news/news.component';
+// import { VolunteerComponent } from './components/volunteer/volunteer.component';
+// import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    // { path: 'about-us', component: AboutUsComponent },
+    // { path: 'about-us/who-we-are', component: WhoWeAreComponent },
+    // { path: 'about-us/faqs', component: FaqsComponent },
+    // { path: 'league', component: LeagueComponent },
+    // { path: 'league/scoreboard', component: ScoreboardComponent },
+    // { path: 'league/recent-match', component: RecentMatchComponent },
+    // { path: 'league/schedule', component: ScheduleComponent },
+    // { path: 'news', component: NewsComponent },
+    // { path: 'volunteer', component: VolunteerComponent },
+    // { path: 'contact-us', component: ContactUsComponent }
 ];
