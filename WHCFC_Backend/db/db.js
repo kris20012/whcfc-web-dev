b6d8c0b7b0e101b79c28bdcb9856c11d194f9a91
Tony Klinpon <thitiklinpon@gmail.com>
2024-10-01 10:31:44 -0400

    Update backend route for agendas

--- WHCFC_Backend/db/db.js
+++ WHCFC_Backend/db/db.js
@@ -2,22 +2,29 @@ import mysql from "mysql2/promise";
 import "dotenv/config";
 import { table_create_query } from "./table_query.js";
 
-// Create connection to MySQL database
-const db = await mysql.createConnection(
-  "mysql://" +
-    process.env.DB_USER +
-    ":" +
-    process.env.DB_PASSWORD +
-    "@" +
-    process.env.DB_HOST +
-    ":" +
-    process.env.DB_PORT +
-    "/" +
-    process.env.DB_NAME
-);
-console.log("Connected to database");
+async function initializeDatabase() {
+  try {
+    // Create connection to MySQL database
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      multipleStatements: true,
+    });
+    console.log("Connected to database");
 
-// Create table if it doesn't exist
-await db.query(table_create_query);
+    // Create table if it doesn't exist
+    await db.query(table_create_query);
+    console.log("Table checked/created");
 
+    return db;
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+    throw error;
+  }
+}
+
+const db = await initializeDatabase();
 export default db;
