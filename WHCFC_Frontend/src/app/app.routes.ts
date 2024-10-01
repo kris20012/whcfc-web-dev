09c3a62ed5767b7ac31dc1840d514259112f5acf
Tony Klinpon <thitiklinpon@gmail.com>
2024-10-01 10:34:27 -0400

    Add event page

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -4,17 +4,18 @@ import { VolunteerComponent } from './pages/volunteer/volunteer.component';
 import { NgModule } from '@angular/core';
 import { FaqComponent } from './pages/faq/faq.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { EventComponent } from './pages/event/event.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'volunteer', component: VolunteerComponent},
-    { path: 'faq', component: FaqComponent},
-    {path: 'contact-us', component: ContactComponent}
+  { path: '', component: HomeComponent },
+  { path: 'volunteer', component: VolunteerComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'contact-us', component: ContactComponent },
+  { path: 'events', component: EventComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-
-  export class AppRoutingModule { }
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
