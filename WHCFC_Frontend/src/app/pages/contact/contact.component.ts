5ea90353b76ae335ae90ca1a25c37d093136715b
Tony Klinpon <thitiklinpon@gmail.com>
2024-10-03 20:20:15 -0400

    Add iframe of Google Map

--- WHCFC_Frontend/src/app/pages/contact/contact.component.ts
+++ WHCFC_Frontend/src/app/pages/contact/contact.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
 import {
   FormGroup,
   FormBuilder,
@@ -11,17 +10,12 @@ import { EmailService } from '../../services/email.service';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, GoogleMapsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
 export class ContactComponent {
   contactForm: FormGroup;
-  options: google.maps.MapOptions = {
-    mapId: 'DEMO_MAP_ID',
-    center: { lat: 43.786335234333514, lng: -79.2497391188189 },
-    zoom: 4,
-  };
   constructor(private emailService: EmailService, private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       firstname: ['', Validators.required],
